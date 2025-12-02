880dccd25e0566cf920bed8c27276b248d45ed84
Harish Gudipudi <94886947+harishsavitri@users.noreply.github.com>
2025-12-02 18:19:13 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ const contactMethods = [
   {
     icon: Linkedin,
     label: "LinkedIn",
-    value: "linkedin.com/in/harish-gudipudi",
+    value: "linkedin.com/harish-gudipudi",
     link: "https://www.linkedin.com/in/harish-gudipudi-b98726178/",
   },
   {
