5298a7dd38af7fd257171f3260567378ed2daf85
Harish Gudipudi <94886947+harishsavitri@users.noreply.github.com>
2025-12-06 18:00:47 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,6 +46,9 @@ const Hero = () => {
                     Architecting robust, scalable cloud solutions with AWS, Kubernetes, and modern DevOps practices.
                     Passionate about automation, CI/CD, and infrastructure as code.    
             </p>
+            <a href="https://drive.google.com/file/d/1knpBXeePmj3sGKwW0xaM_DZc6Q-BaTWO/view?usp=drive_link" class="cta">Checkout my Resume</a>
+                
+                    
             <div className="flex flex-wrap gap-4 justify-center lg:justify-start pt-4">
               <Button 
                 size="lg" 
