2796d6918e644fc224a8fa6bf9eae8176bcf11dd
Harish Gudipudi <94886947+harishsavitri@users.noreply.github.com>
2025-12-07 11:33:22 +0530

    Update Achivements.tsx

--- src/components/Achivements.tsx
+++ src/components/Achivements.tsx
@@ -11,7 +11,7 @@ const achievements = [
   {
     title: "Python Certification - Kelly Technologies",
     description:
-      "Completed Python certification from Kelly Technologies, gaining hands-on experience in data manipulation, visualization, and solving real-world problems using Python in an interactive learning environment.",
+      "Completed Python certification from KAGGLE Technologies, gaining hands-on experience in data manipulation, visualization, and solving real-world problems using Python in an interactive learning environment.",
     link: "https://drive.google.com/file/d/1oULC6vjcyEL32kAldj4Ava3P82F01dLX/view?usp=sharing",
   },
   {
