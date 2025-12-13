333d548900c560dc612dabad576769ff3d717266
Harish Gudipudi <94886947+harishsavitri@users.noreply.github.com>
2025-12-13 09:43:38 +0530

    Update DemoVideo.tsx

--- src/components/DemoVideo.tsx
+++ src/components/DemoVideo.tsx
@@ -30,14 +30,14 @@ const DemoVideo = () => {
             <video
               controls
               className="w-full h-full object-cover"
-              poster="https://unsplash.com/photos/a-woman-sitting-on-a-log-on-the-beach-iNwfKCxVE9g"
+              poster="https://plus.unsplash.com/premium_photo-1732835448105-a68fd08e9bb0?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             >
               <source src="https://github.com/harishsavitri/Harish-Gudipudi-portfolio-hub/blob/main/natureworld.mp4?raw=true" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
           <p className="text-center text-sm text-muted-foreground mt-4">
-            Demo:  It's a Pub Time (Zero40 Trivia Nights – Zero40) | Jubilee Hills– Hyderabad 271/A, Road Number 10.
+            Demo:  4K Ultra HD Nature Film | Relaxing Cinematic Documentary of Earth’s Wild Places.
           </p>
           <div className="relative aspect-video rounded-2xl overflow-hidden border-2 border-accent/30 shadow-glow-accent bg-card">
             <video
